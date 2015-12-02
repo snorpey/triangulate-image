b18e17e9b43f45669e4a2b88e630bec85a3577ab
Georg Fischer <snorpey@gmail.com>
2015-12-03 00:17:33 +0100

    move params

--- trianglulation.js
+++ trianglulation.js
@@ -11,16 +11,7 @@ var getSVGMarkupFromTriangleData = require( './getSVGMarkupFromTriangleData.js' 
 var getDataURL = require( './getDataURL.js' );
 var Image = Canvas.Image;
 
-
-var values = {
-	blur: 50,         // can be whatever
-	accuracy: 0.2,    // between 0 - 1
-	pointRate: 0.03,  // between 0.001 - 0.1
-	pointCount: 1200  // can be whatever
-};
-
-
-function triangulateImage ( imgPath, callback ) {
+function triangulateImage ( imgPath, params, callback ) {
 	fs.readFile( __dirname + '/' + imgPath, function ( err, src ){
 		if ( err ) {
 			throw err;
@@ -35,12 +26,12 @@ function triangulateImage ( imgPath, callback ) {
 		var imageData = ctx.getImageData( 0, 0, canvas.width, canvas.height );
 		var colorData = ctx.getImageData( 0, 0, canvas.width, canvas.height );
 
-		var blurredImageData = superfastBlur( imageData, values.blur, false );
+		var blurredImageData = superfastBlur( imageData, params.blur, false );
 		var greyscaleImageData = greyscale( blurredImageData );
 		var edgesImageData = detectEdges( greyscaleImageData );
 
-		var edgePoints = getEdgePoints( edgesImageData, 50, values.accuracy );
-		var edgeVertices = getRandomVertices( edgePoints, values.pointRate, values.pointCount, values.accuracy, canvas.width, canvas.height );
+		var edgePoints = getEdgePoints( edgesImageData, 50, params.accuracy );
+		var edgeVertices = getRandomVertices( edgePoints, params.pointRate, params.pointCount, params.accuracy, canvas.width, canvas.height );
 
 		var polygons = delaunay.triangulate( edgeVertices );
 		var triangles = getTrianglesWithColor( polygons, colorData );
@@ -51,7 +42,14 @@ function triangulateImage ( imgPath, callback ) {
 	} );
 }
 
-triangulateImage( 'images/lincoln.jpg', function ( triangles, size ) {
+var configParams = {
+	blur: 50,         // can be whatever
+	accuracy: 0.2,    // between 0 - 1
+	pointRate: 0.03,  // between 0.001 - 0.1
+	pointCount: 1200  // can be whatever
+};
+
+triangulateImage( 'images/lincoln.jpg', configParams, function ( triangles, size ) {
 	var svgMarkup = getSVGMarkupFromTriangleData( triangles, size );
 	console.log( 'triangulated SVG markup', svgMarkup );
 
