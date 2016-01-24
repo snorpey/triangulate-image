4c8ef0e1c8b5d4c092faec34decca4b372745ef4
Georg Fischer <snorpey@gmail.com>
2016-01-24 21:25:43 +0100

    fix copyImageData for browsers withozt Uint8ClampedArray

--- src/imagedata/copyImageData.js
+++ src/imagedata/copyImageData.js
@@ -1,7 +1,21 @@
+var Canvas = require('canvas-browserify');
+
 module.exports = function ( imageData ) {
-	return {
-		width: imageData.width,
-		height: imageData.height,
-		data: new Uint8ClampedArray( imageData.data )
-	};
+	// this is mainly required to run the browser tests.
+	// phantomjs < v2 doesn't understand Uint8ClampedArray 
+	if ( typeof Uint8ClampedArray === 'undefined' ) {
+		// http://stackoverflow.com/a/11918126/229189
+		var canvas = Canvas( imageData.width, imageData.height );
+		var ctx = canvas.getContext( '2d' );
+		ctx.putImageData( imageData, 0, 0 );
+		
+		return ctx.getImageData( 0, 0, imageData.width, imageData.height );
+	} else {
+		// http://stackoverflow.com/a/15238036/229189
+		return {
+			width: imageData.width,
+			height: imageData.height,
+			data: new Uint8ClampedArray( imageData.data )
+		};
+	}
 }
