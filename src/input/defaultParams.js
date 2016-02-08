84e7e38cc5aebb1f4b2aa5cb97b16d7fb4f0f085
Georg Fischer <snorpey@gmail.com>
2016-02-08 11:26:16 +0100

    update default params

--- src/input/defaultParams.js
+++ src/input/defaultParams.js
@@ -1,9 +1,10 @@
 export default {
 	accuracy: 0.7,
-	blur: 40,
+	blur: 4,
 	fill: true,
 	stroke: true,
 	strokeWidth: 0.5,
 	lineJoin: 'miter',
-	vertexCount: 700
+	vertexCount: 700,
+	threshold: 50
 };
