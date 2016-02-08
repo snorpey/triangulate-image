eeccd52aed56be59520e60ef2a8c8f189c417579
Georg Fischer <snorpey@gmail.com>
2016-02-08 11:25:56 +0100

    remove access issue in greyscale code

--- src/imagedata/greyscale.js
+++ src/imagedata/greyscale.js
@@ -1,17 +1,14 @@
 export default function ( imageData ) {
 	let len = imageData.data.length;
-	let data = imageData.data;
 	let brightness;
 
 	for ( let i = 0; i < len; i += 4 ) {
-		brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
+		brightness = 0.34 * imageData.data[i] + 0.5 * imageData.data[i + 1] + 0.16 * imageData.data[i + 2];
 
-		data[i] = brightness;
-		data[i + 1] = brightness;
-		data[i + 2] = brightness;
+		imageData.data[i] = brightness;
+		imageData.data[i + 1] = brightness;
+		imageData.data[i + 2] = brightness;
 	}
-
-	imageData.data = data;
-	
+		
 	return imageData;
 }
