e9f4abd109838da61ba2df55abd6828002348ea8
Georg Fischer <snorpey@gmail.com>
2016-02-08 11:11:45 +0100

    replace edge detection algorithm with sobel filter

--- src/polygons/imageDataToPolygons.js
+++ src/polygons/imageDataToPolygons.js
@@ -1,11 +1,10 @@
-'use strict';
 import stackBlur from 'stackblur-canvas';
 import delaunay from 'delaunay-fast';
+import Sobel from 'sobel';
 
 import isImageData from '../util/isImageData';
 import copyImageData from '../imagedata/copyImageData';
 import greyscale from '../imagedata/greyscale';
-import detectEdges from '../imagedata/detectEdges';
 import getEdgePoints from './getEdgePoints';
 import getVerticesFromPoints from './getVerticesFromPoints';
 import addBoundingBoxesToPolygons from './addBoundingBoxesToPolygons';
@@ -15,14 +14,12 @@ import addGradientsToPolygons from './addGradientsToPolygons';
 export default function ( imageData, params ) {
 	if ( isImageData( imageData ) ) {
 		let imageSize = { width: imageData.width, height: imageData.height };
-
 		let tmpImageData = copyImageData( imageData );
 		let colorImageData = copyImageData( imageData );
-		
 		let blurredImageData = stackBlur.imageDataRGBA( tmpImageData, 0, 0, imageSize.width, imageSize.height, params.blur );
 		let greyscaleImageData = greyscale( blurredImageData );
-		let edgesImageData = detectEdges( greyscaleImageData );
-		let edgePoints = getEdgePoints( edgesImageData, 50, params.accuracy );
+		let edgesImageData = Sobel( greyscaleImageData ).toImageData();
+		let edgePoints = getEdgePoints( edgesImageData, params.threshold );
 		let edgeVertices = getVerticesFromPoints( edgePoints, params.vertexCount, params.accuracy, imageSize.width, imageSize.height );
 		let polygons = delaunay.triangulate( edgeVertices );
 		
