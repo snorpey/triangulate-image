b846e294483302919e7b87521f2c49ac488ecfb7
Georg Fischer <hi@snorpey.com>
2020-04-22 21:58:07 +0200

    build

--- dist/triangulate-image-node.js
+++ dist/triangulate-image-node.js
@@ -2,9 +2,9 @@
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('stream')) :
 	typeof define === 'function' && define.amd ? define(['stream'], factory) :
 	(global = global || self, global.triangulate = factory(global.stream));
-}(this, function (stream) { 'use strict';
+}(this, (function (stream) { 'use strict';
 
-	stream = stream && stream.hasOwnProperty('default') ? stream['default'] : stream;
+	stream = stream && Object.prototype.hasOwnProperty.call(stream, 'default') ? stream['default'] : stream;
 
 	function clamp ( value, min, max ) {
 		return value < min ? min : value > max ? max : value;
@@ -245,7 +245,6 @@
 			return ctx.getImageData( 0, 0, canvas.width, canvas.height );
 		} else {
 			throw new Error( "Can't work with the buffer object provided." );
-			return;
 		}
 	}
 
@@ -363,7 +362,7 @@
 		
 		var canvasData = makeCanvasAndContext( size, options, dpr, format );
 		
-		drawPolygonsOnContext( canvasData.ctx, polygons, size );
+		drawPolygonsOnContext( canvasData.ctx, polygons);
 
 		return canvasData.canvas.toBuffer();
 	}
@@ -554,8 +553,6 @@
 
 		return canvas.jpegStream( streamParams );
 	}
-
-	var commonjsGlobal = typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
 	function createCommonjsModule(fn, module) {
 		return module = { exports: {} }, fn(module, module.exports), module.exports;
@@ -872,13 +869,13 @@
 	  }
 
 	  {
-	    if (module.exports) {
+	    if ( module.exports) {
 	      exports = module.exports = Sobel;
 	    }
 	    exports.Sobel = Sobel;
 	  }
 
-	})(commonjsGlobal);
+	})();
 	});
 	var sobel_1 = sobel.Sobel;
 
@@ -898,7 +895,6 @@
 			if ( typeof Uint8ClampedArray === 'undefined' ) {
 				if ( typeof window === 'undefined' ) {
 					throw new Error( "Can't copy imageData in webworker without Uint8ClampedArray support." );
-					return imageData;
 				} else {
 					return copyImageDataWithCanvas( imageData );
 				}
@@ -921,7 +917,6 @@
 					} catch ( err ) {
 						if ( typeof window === 'undefined' ) {
 							throw new Error( "Can't copy imageData in webworker without proper ImageData() support." );
-							result = imageData;
 						} else {
 							result = copyImageDataWithCanvas( imageData );
 						}
@@ -932,7 +927,6 @@
 			}
 		} else {
 			throw new Error( 'Given imageData object is not useable.' );
-			return;
 		}
 	}
 
@@ -1707,7 +1701,6 @@
 			return polygons;
 		} else {
 			throw new Error( "Can't work with the imageData provided. It seems to be corrupt." );
-			return;
 		}
 	}
 
@@ -1937,4 +1930,4 @@
 
 	return index;
 
-}));
+})));
